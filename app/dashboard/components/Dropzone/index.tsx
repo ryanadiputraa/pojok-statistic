3e7d4630fcd786957894b59af354da2cf7bb7897
Ryan Adi Putra <ryannadiputraa@gmail.com>
2022-11-29 16:43:49 +0800

    feat: drag & drop file

---
+++ app/dashboard/components/Dropzone/index.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import React, { Dispatch, SetStateAction, useCallback, useState } from "react";
+import { useDropzone, Accept } from "react-dropzone";
+
+interface Props {
+  setFile: Dispatch<SetStateAction<File | null>>;
+  isInvalidFormat: boolean;
+  setIsInvalidFormat: Dispatch<SetStateAction<boolean>>;
+}
+
+const acceptedFileExtension = ["xls", "xlsx", "xlsb"];
+
+export default function FileDropzone({
+  setFile,
+  isInvalidFormat,
+  setIsInvalidFormat,
+}: Props) {
+  const [fileName, setFileName] = useState<string>("");
+
+  const [onDrag, setOnDrag] = useState<boolean>(false);
+
+  const onDrop = useCallback(<T extends File>(acceptedFiles: T[]) => {
+    const fileExt = acceptedFiles[0].name.split(".").pop() ?? "";
+    setOnDrag(false);
+    if (acceptedFileExtension.includes(fileExt)) {
+      setFile(acceptedFiles[0]);
+      setFileName(acceptedFiles[0].name);
+      setIsInvalidFormat(false);
+    } else {
+      setFileName("");
+      setIsInvalidFormat(true);
+    }
+  }, []);
+
+  const onDragEnter = useCallback(() => setOnDrag(true), []);
+  const onDragLeave = useCallback(() => setOnDrag(false), []);
+
+  const { getRootProps, getInputProps } = useDropzone({
+    onDrop,
+    onDragEnter,
+    onDragLeave,
+    multiple: false,
+  });
+
+  return (
+    <div
+      {...getRootProps({
+        className: `cursor-pointer bg-secondary h-80 flex flex-col justify-center items-center ${
+          onDrag ? "border-accent" : "border-greyLight"
+        }  border-4 border-dashed`,
+      })}
+    >
+      <input {...getInputProps()} />
+      <span className="italic font-montserratBold">
+        Drag and drop your file or click here to upload
+      </span>
+      {isInvalidFormat && (
+        <p className="text-red-500">
+          Please use valid file format, you can use our template by clicking
+          <span className="font-montserratBold"> "Download Template" </span>
+          button above
+        </p>
+      )}
+      <span className="text-accent">{fileName}</span>
+    </div>
+  );
+}
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,16 +1,56 @@
+"use client";
+
+import { useState } from "react";
+
+import FileDropzone from "dashboard/components/Dropzone";
+
 export default function Dashboard() {
+  const [file, setFile] = useState<File | null>(null);
+  const [isInvalidFormat, setIsInvalidFormat] = useState<boolean>(false);
+
+  const onAnalyze = async () => {
+    if (!file) return;
+    const formData = new FormData();
+    formData.append("file", file);
+
+    try {
+      const resp = await fetch("http://localhost:8080/api/turnover", {
+        method: "POST",
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+        body: formData,
+      });
+      console.log(resp);
+    } catch (error) {
+      console.error(error);
+      // REMOVE LATER
+      alert("API BELUM JALAN PAK");
+    }
+  };
+
   return (
-    <div className="w-full">
+    <div className="w-full flex flex-col">
       <div className="flex justify-between items-center">
         <h4>Analyze your company turnover by using our template!</h4>
-        <a
-          className="bg-accent text-secondary p-2 rounded-md cursor-pointer font-montserratBold shadow-xl hover:brightness-95"
-          href="favicon.ico"
-          download
-        >
+        <a className="main-btn" href="template.xlsx" download>
           Download Template
         </a>
       </div>
+      <FileDropzone
+        setFile={setFile}
+        isInvalidFormat={isInvalidFormat}
+        setIsInvalidFormat={setIsInvalidFormat}
+      />
+      <button
+        disabled={!file || isInvalidFormat}
+        className={`main-btn self-center mt-2 ${
+          !file || isInvalidFormat ? "grayscale" : ""
+        }`}
+        onClick={onAnalyze}
+      >
+        Analyze
+      </button>
     </div>
   );
 }
