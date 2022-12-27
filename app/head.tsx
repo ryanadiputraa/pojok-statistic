93f257b316d3c0b98190b8a5c6e5e0a36376f338
Ryan Adi Putra <ryannadiputraa@gmail.com>
2022-12-27 09:40:24 +0800

    fix: mixed content security meta

--- app/head.tsx
+++ app/head.tsx
@@ -3,6 +3,7 @@ export default function Head() {
     <>
       <title>Pojok Statistik</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
       <link rel="icon" href="/ico.png" />
     </>
   );
